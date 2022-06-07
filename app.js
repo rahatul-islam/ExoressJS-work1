0c7bbf6a74961dcc1af09bd2706306ada2af581f
Rahat <mdrahatulislam121@.com>
2022-06-07 10:58:20 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const contactRoutes = require('./contactRoutes')
+
+const app = express()
+app.use(morgan('dev'))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+app.use('/contacts', contactRoutes)
+
+app.get('*', (req, res) => {
+    res.send('<h1>Please use the Correct Routes</h1>')
+})
+
+const PORT = process.env.PORT || 7070
+app.listen(PORT, () => {
+    console.log(`Server is Running on PORT ${PORT}`);
+})
---
+++ contactRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+
+const {
+    getAllContacts,
+    createContact,
+    getContactById,
+    updateContact,
+    deleteContact
+} = require('./contactControllar')
+
+router.get('/', getAllContacts)
+router.post('/', createContact)
+router.get('/:id', getContactById)
+router.put('/:id', updateContact)
+router.delete('/:id', deleteContact)
+
+module.exports = router
